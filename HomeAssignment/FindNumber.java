9b06b4ff15dc506e2e2bf6cafdba640968891041
rajashree-1997 <155864204+rajashree-1997@users.noreply.github.com>
2024-01-07 08:54:48 +0530

    Add files via upload

---
+++ HomeAssignment/FindNumber.java
@@ -0,0 +1,17 @@
+package HomeAssignment;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		int number = 45;
+		// if=ctrl+space
+		if (number > 0) {
+			System.out.println("number is positive");
+		} else {
+			System.out.println("number is negative");
+
+		}
+
+	}
+
+}
